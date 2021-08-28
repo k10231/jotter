6ccae44d46dfc2e2c89ba83239d8576f6d2956a4
a d <84557399+akhmed-codes@users.noreply.github.com>
2021-08-28 17:22:18 +0530

    Create bash.sh

---
+++ cli-runner/bash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -X POST 'https://hurrxycxigvviayjhlxr.supabase.co/rest/v1/jus>
+-H "apikey:...>
+-H "Authorization:Bearer ...>
+-H "Content-Type:application/json" \
+-H "Prefer:return=representation" \
+-d '{ "stuff": "'"$1"'" }'
